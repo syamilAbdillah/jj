f466ed41f7d278a5a241c47e99563c927f7c8b09
syamilAbdillah <abdillahsyamil77@gmail.com>
2024-11-08 10:04:12 +0700

    test: add more test

--- jj_test.go
+++ jj_test.go
@@ -2,6 +2,7 @@ package jj
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -54,7 +55,30 @@ func TestMultipleAttributes(t *testing.T) {
 	New(&b).CustomElement("div", Attrs{"class": "foo", "id": "bar"}, func(j J) {
 		j.Text("Hello")
 	})
-	if b.String() != "<div class=\"foo\" id=\"bar\">Hello</div>" {
+	if b.String() != "<div class=\"foo\" id=\"bar\">Hello</div>" || b.String() != "<div id=\"bar\" class=\"foo\">Hello</div>" {
 		t.Errorf("Expected <div class=\"foo\" id=\"bar\">Hello</div>, got %s", b.String())
+	}
+}
+
+func TestLooping(t *testing.T) {
+	var b bytes.Buffer
+	New(&b).Div(nil, func(j J) {
+		for i := 0; i < 10; i++ {
+			j.Span(nil, func(j J) {
+				j.Text(fmt.Sprintf("%d", i))
+			})
+		}
+	})
+	expected := "<div><span>0</span><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span><span>7</span><span>8</span><span>9</span></div>"
+	if b.String() != expected {
+		t.Errorf("Expected %s, got %s", expected, b.String())
+	}
+}
+
+func TestNilAttributes(t *testing.T) {
+	var b bytes.Buffer
+	New(&b).Input(Attrs{"required": nil})
+	if b.String() != "<input required/>" {
+		t.Errorf("Expected <input required/> got %s", b.String())
 	}
 }
